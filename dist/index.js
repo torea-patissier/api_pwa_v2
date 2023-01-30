db868e239b27aca6a842b517b01aaa14e1e9ebb8
Torea Patissier <p.torea@icloud.com>
2023-01-30 23:44:34 +0100

    update index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,66 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const AWS = __importStar(require("aws-sdk"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({ region: process.env.REGION });
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.send('Express +  COUCOU TypeScript Server');
 });
+app.post('/user/register/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password, name } = req.body;
+    try {
+        const params = {
+            ClientId: `${process.env.CLIENT_ID}`,
+            Password: password,
+            Username: email,
+            UserAttributes: [{ Name: 'name', Value: name }],
+        };
+        yield cognitoIdentityServiceProvider.signUp(params).promise();
+        res.json({ message: 'User registered' });
+    }
+    catch (err) {
+        res.status(400).json({ message: err });
+    }
+}));
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- index.ts
+++ index.ts
@@ -1,13 +1,34 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import * as AWS from 'aws-sdk';
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({region: process.env.REGION});
+
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('Express +  COUCOU TypeScript Server');
+});
+app.post('/user/register/', async (req: Request, res: Response) => {
+  const {email, password, name} = req.body;
+
+  try {
+    const params = {
+      ClientId: `${process.env.CLIENT_ID}`,
+      Password: password,
+      Username: email,
+      UserAttributes: [{Name: 'name', Value: name}],
+    };
+    await cognitoIdentityServiceProvider.signUp(params).promise();
+
+    res.json({message: 'User registered'});
+  } catch (err) {
+    res.status(400).json({message: err});
+  }
 });
 
 app.listen(port, () => {
